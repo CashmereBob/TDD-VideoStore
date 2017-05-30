13005cb461a5ffb1f78a8e8d6bbd0a26dc88ddb9
Magnus Weidmar <weidmar@gmail.com>
2017-05-30 15:21:35 +0200

    Interfaces and classes added

---
+++ TDD-VideoStore/VideoStore.BL/Rentals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore.BL
+{
+    class Rentals : IRentals
+    {
+        public void AddRental(string movieTitle, string socialSecurityNumber)
+        {
+        }
+
+        public List<Rental> GetRentalsFor(string socialSecurityNumber)
+        {
+            return null;
+        }
+
+        public void RemoveRental(string movieTitle, string socialSecurityNumber)
+        {
+        }
+    }
+}

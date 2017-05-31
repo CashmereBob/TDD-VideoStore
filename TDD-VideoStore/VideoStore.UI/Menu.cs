3092b70cbb281b1486a88faa7e8d144a6ab27568
Roppin.L <robin.ljungqvist@gmail.com>
2017-05-31 13:13:20 +0200

    began building UI

---
+++ TDD-VideoStore/VideoStore.UI/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore.UI
+{
+    public class Menu
+    {
+        private bool IsRunning { get; set; } = true;
+        public void Run()
+        {
+            while (IsRunning)
+            {
+                PrintOverallMenu();
+                var userChoice = Console.ReadKey();
+                var key = userChoice.Key;
+                switch (key)
+                {
+                    // Rent Movie
+                    case ConsoleKey.NumPad1:
+                        break;
+
+                    // Return Movie
+                    case ConsoleKey.NumPad2:
+                        break;
+
+                    // Get Movies
+                    case ConsoleKey.NumPad3:
+                        break;
+
+                    // Get Customers
+                    case ConsoleKey.NumPad4:
+                        break;
+                    
+                    // Register Customer
+                    case ConsoleKey.NumPad5:
+                        break;
+
+                    // Add Movie
+                    case ConsoleKey.NumPad6:
+                        break;
+                    case ConsoleKey.NumPad9:
+                        Environment.Exit(-1);
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+
+        private void PrintOverallMenu()
+        {
+            Console.WriteLine("Welcome to the VideoStore");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("1. Rent Movie");
+            Console.WriteLine("2. Return Movie");
+            Console.WriteLine("3. Get Movies");
+            Console.WriteLine("4. Get Customers");
+            Console.WriteLine("5. Register Customer");
+            Console.WriteLine("6. Add Movie");
+            Console.WriteLine("9. Exit");
+
+
+
+        }
+    }
+}
